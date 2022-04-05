f4479d18599062f81f2154df4f9d9b566690daf2
Stepanenko Valdimyr <vova.stepanenko2020@gmail.com>
2022-04-05 22:52:04 +0300

    separate  properties from java file to database.properties file

--- src/main/java/chat/org/util/ConnectionUtil.java
+++ src/main/java/chat/org/util/ConnectionUtil.java
@@ -1,32 +1,42 @@
 package chat.org.util;
 
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/chat?serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "20152015aA!";
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final Properties dbProp;
 
     static {
+        dbProp = loadProperties();
+
         try {
-            Class.forName(JDBC_DRIVER);
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't find SQL Driver", e);
         }
     }
 
     public static Connection getConnection() {
-        Properties dbProperties = new Properties();
-        dbProperties.setProperty("user", USERNAME);
-        dbProperties.setProperty("password", PASSWORD);
         try {
-            return DriverManager.getConnection(URL, dbProperties);
+            return DriverManager.getConnection(dbProp.getProperty("url"), dbProp);
         } catch (SQLException e) {
             throw new RuntimeException("Can't create connection to DB ", e);
         }
+    }
+
+    private static Properties loadProperties() {
+        Properties dbProp = new Properties();
+        try (InputStream input = ConnectionUtil.class.getClassLoader().getResourceAsStream("database.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Sorry, unable to find database.properties");
+            }
+            dbProp.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dbProp;
     }
 }
