177d813316030a40cd3eacf48cdc549ce7a7087a
Vova Stepanenko <vova.stepanenko2020@gmail.com>
2022-04-05 17:12:19 +0300

    del

--- src/main/java/chat/dto/MessageDto.java
+++ src/main/java/chat/dto/MessageDto.java
@@ -8,4 +8,20 @@ public class MessageDto {
         this.text = text;
         this.userName = userName;
     }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
